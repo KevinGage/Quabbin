0a8b1fb644dc8f54cad699c58accf56d4a6876da
KevinGage <dudeman02379@gmail.com>
2020-09-29 21:15:55 -0400

    created initial routes

--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavbarComponent from './components/NavbarComponent/NavbarComponent';
+import Records from './components/Records/Records';
+import RawData from './components/RawData/RawData';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <NavbarComponent />
+        <Switch>
+          <Route exact path="/">
+            <Records />
+          </Route>
+          <Route path="/data">
+            <RawData />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
