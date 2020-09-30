76341b575c1aa65e3b605186adea6bfb739a304e
KevinGage <dudeman02379@gmail.com>
2020-09-29 21:15:48 -0400

    created placeholder components

---
+++ website/src/components/NavbarComponent/NavbarComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function NavbarComponent() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default NavbarComponent;
---
+++ website/src/components/Records/Records.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Records() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Records;
