0d4ee58bdde72e5c73d4d6b812fd3644c6d34415
KevinGage <dudeman02379@gmail.com>
2020-09-30 21:15:36 -0400

    started making navbar

--- website/src/components/NavbarComponent/NavbarComponent.js
+++ website/src/components/NavbarComponent/NavbarComponent.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import QuabbinThumbnail from '../../images/QuabbinThumbnail.png';
 
 function NavbarComponent() {
   return (
-    <div>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+      <Navbar.Brand href="/">
+        <img src={QuabbinThumbnail} alt='logo'/>
+        Quabbin Classic
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Records</Nav.Link>
+          <Nav.Link href="/data">Spreadsheet</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+  </Navbar>
   );
 }
 
