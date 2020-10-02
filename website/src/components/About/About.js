d58c326b2513116a70774d092334a2c1090fb547
KevinGage <dudeman02379@gmail.com>
2020-10-01 22:40:32 -0400

    added placeholders on about page

--- website/src/components/About/About.js
+++ website/src/components/About/About.js
@@ -3,6 +3,14 @@ import React from 'react';
 function About() {
   return (
     <div>
+      <h1>About the Quabbin Classic</h1>
+      <p></p>
+      <hr />
+      <h1>About the data</h1>
+      <p></p>
+      <hr />
+      <h1>About this website</h1>
+      <p></p>
     </div>
   );
 }
