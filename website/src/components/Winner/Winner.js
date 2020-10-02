03c8ba997d488f01c39b9f0e2ef23473f7ad6034
KevinGage <dudeman02379@gmail.com>
2020-10-01 23:02:39 -0400

    continued styling records

--- website/src/components/Winner/Winner.js
+++ website/src/components/Winner/Winner.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Winner.module.css';
+import classNames from 'classnames/bind';
 
 function Winner({place, name, year, info}) {
+  const cx = classNames.bind(styles);
+
   return (
-    <div className={styles.winner}>
-      {place}<br />
+    <div className={cx(styles.winner, {
+      first: place === 1,
+      second: place === 2,
+      third: place === 3
+    })}
+    >
       {name}<br />
       {year}<br />
       {info.map((info) => {
