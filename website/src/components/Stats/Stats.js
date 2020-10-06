5bf6b3dc2e4f1a49367cff850bd0133b3ee5621c
KevinGage <dudeman02379@gmail.com>
2020-10-06 14:29:20 -0400

    added chart that shows number of fish per year by species

--- website/src/components/Stats/Stats.js
+++ website/src/components/Stats/Stats.js
@@ -44,6 +44,18 @@ function Stats({fish}) {
     return a;
   }, []);
 
+  const fishCountPerYear = averageFishWeightPerYear.map(x => {
+    return {
+      id: x.id,
+      data: x.data.map(y => {
+        return {
+          x: y.x,
+          y: y.Count
+        }
+      })
+    }
+  })
+
   return (
     <Container fluid>
       <div className="d-flex flex-wrap justify-content-center">
@@ -59,6 +71,53 @@ function Stats({fish}) {
             // tickPadding: 5,
             // tickRotation: 0,
             legend: 'Average Weight Of Fish Per Year By Species',
+            legendOffset: 36,
+            legendPosition: 'middle'
+          }}
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemBackground: 'rgba(0, 0, 0, .03)',
+                    itemOpacity: 1
+                  }
+                }
+              ]
+            }
+          ]}
+          isInteractive={true}
+          useMesh={true}
+        />
+        </div>
+      </div>
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className={styles.chartContainer}>
+        <ResponsiveLine
+          data={fishCountPerYear}
+          margin={{ top: 55, right: 165, bottom: 55, left: 25 }}
+          curve="monotoneX"
+          colors={{ scheme: 'paired' }}
+          axisBottom={{
+            orient: 'bottom',
+            // tickSize: 5,
+            // tickPadding: 5,
+            // tickRotation: 0,
+            legend: 'Number Of Fish Per Year By Species',
             legendOffset: 36,
             legendPosition: 'middle'
           }}
