3500a2f9c39e1882d733d0ae2df6b2cf56f2012c
KevinGage <dudeman02379@gmail.com>
2020-10-07 16:07:39 -0400

    added example pie chart

--- website/src/components/Stats/Stats.js
+++ website/src/components/Stats/Stats.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './Stats.module.css';
 import { Container } from 'react-bootstrap';
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveLine } from '@nivo/line';
+import { ResponsivePie } from '@nivo/pie';
 
 function Stats({fish}) {
   // Just use Oz to make things easier for now
@@ -56,100 +57,124 @@ function Stats({fish}) {
     }
   })
 
+  const totalFishCounts = fishCountPerYear.map(x => {
+    return {
+      'id': x.id,
+      'label': x.id,
+      'value': x.data.reduce((a,c) => {
+        return a += parseInt(c.y)
+      }, 0)
+    }
+  });
+
   return (
     <Container fluid>
       <div className="d-flex flex-wrap justify-content-center">
-        <div className={styles.chartContainer}>
-        <ResponsiveLine
-          data={averageFishWeightPerYear}
-          margin={{ top: 55, right: 165, bottom: 55, left: 25 }}
-          curve="monotoneX"
-          colors={{ scheme: 'paired' }}
-          axisBottom={{
-            orient: 'bottom',
-            // tickSize: 5,
-            // tickPadding: 5,
-            // tickRotation: 0,
-            legend: 'Average Weight Of Fish Per Year By Species',
-            legendOffset: 36,
-            legendPosition: 'middle'
-          }}
-          legends={[
-            {
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: 'left-to-right',
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: 'circle',
-              symbolBorderColor: 'rgba(0, 0, 0, .5)',
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemBackground: 'rgba(0, 0, 0, .03)',
-                    itemOpacity: 1
+        <div className={styles.lineContainer}>
+          <ResponsiveLine
+            data={averageFishWeightPerYear}
+            margin={{ top: 55, right: 165, bottom: 55, left: 25 }}
+            curve="monotoneX"
+            colors={{ scheme: 'paired' }}
+            axisBottom={{
+              orient: 'bottom',
+              // tickSize: 5,
+              // tickPadding: 5,
+              // tickRotation: 0,
+              legend: 'Average Weight Of Fish Per Year By Species',
+              legendOffset: 36,
+              legendPosition: 'middle'
+            }}
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemBackground: 'rgba(0, 0, 0, .03)',
+                      itemOpacity: 1
+                    }
                   }
-                }
-              ]
-            }
-          ]}
-          isInteractive={true}
-          useMesh={true}
-        />
+                ]
+              }
+            ]}
+            isInteractive={true}
+            useMesh={true}
+          />
         </div>
       </div>
       <div className="d-flex flex-wrap justify-content-center">
-        <div className={styles.chartContainer}>
-        <ResponsiveLine
-          data={fishCountPerYear}
-          margin={{ top: 55, right: 165, bottom: 55, left: 25 }}
-          curve="monotoneX"
-          colors={{ scheme: 'paired' }}
-          axisBottom={{
-            orient: 'bottom',
-            // tickSize: 5,
-            // tickPadding: 5,
-            // tickRotation: 0,
-            legend: 'Number Of Fish Per Year By Species',
-            legendOffset: 36,
-            legendPosition: 'middle'
-          }}
-          legends={[
-            {
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 100,
-              translateY: 0,
-              itemsSpacing: 0,
-              itemDirection: 'left-to-right',
-              itemWidth: 80,
-              itemHeight: 20,
-              itemOpacity: 0.75,
-              symbolSize: 12,
-              symbolShape: 'circle',
-              symbolBorderColor: 'rgba(0, 0, 0, .5)',
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemBackground: 'rgba(0, 0, 0, .03)',
-                    itemOpacity: 1
+        <div className={styles.lineContainer}>
+          <ResponsiveLine
+            data={fishCountPerYear}
+            margin={{ top: 55, right: 165, bottom: 55, left: 25 }}
+            curve="monotoneX"
+            colors={{ scheme: 'paired' }}
+            axisBottom={{
+              orient: 'bottom',
+              // tickSize: 5,
+              // tickPadding: 5,
+              // tickRotation: 0,
+              legend: 'Number Of Fish Per Year By Species',
+              legendOffset: 36,
+              legendPosition: 'middle'
+            }}
+            legends={[
+              {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemBackground: 'rgba(0, 0, 0, .03)',
+                      itemOpacity: 1
+                    }
                   }
-                }
-              ]
-            }
-          ]}
-          isInteractive={true}
-          useMesh={true}
-        />
+                ]
+              }
+            ]}
+            isInteractive={true}
+            useMesh={true}
+          />
+        </div>
+      </div>
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className="d-flex flex-column justify-content-center">
+          <div className={styles.pieContainer}>
+            <ResponsivePie
+              data={totalFishCounts}
+              colors={{ scheme: 'paired' }}
+              margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
+              borderWidth={3}
+              borderColor={{ theme: 'background' }}
+            />
+          </div>
+          <p className={styles.label}>Total Number Of Fish By Species</p>
         </div>
       </div>
     </Container>
