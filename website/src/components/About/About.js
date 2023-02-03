7b1e7907d13ecd8eb9b0b3347a0389b2022769f4
KevinGage <dudeman02379@gmail.com>
2023-02-03 10:01:06 -0500

    testing ghpages

--- website/src/components/About/About.js
+++ website/src/components/About/About.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <div>
       <h1>About the Quabbin Classic</h1>
-      <p></p>
-      <hr />
-      <h1>About the data</h1>
       <p></p>
       <hr />
       <h1>About this website</h1>
