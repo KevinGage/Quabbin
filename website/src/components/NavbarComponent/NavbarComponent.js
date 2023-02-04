9df6110314aa22a7663a25a438383df856f37167
KevinGage <dudeman02379@gmail.com>
2023-02-04 12:05:34 -0500

    fixed nav links for hash router

--- website/src/components/NavbarComponent/NavbarComponent.js
+++ website/src/components/NavbarComponent/NavbarComponent.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
-import QuabbinThumbnail from '../../images/QuabbinThumbnail.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import QuabbinThumbnail from "../../images/QuabbinThumbnail.png";
 
 function NavbarComponent() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="light"
+      variant="light"
+      sticky="top"
+    >
       <Navbar.Brand href="/">
-        <img src={QuabbinThumbnail} alt='logo'/>
+        <img src={QuabbinThumbnail} alt="logo" />
         Quabbin Classic
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Records</Nav.Link>
-          <Nav.Link href="/data">Spreadsheet</Nav.Link>
-          <Nav.Link href="/stats">Stats</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Records
+          </Nav.Link>
+          <Nav.Link as={Link} to="/data">
+            Spreadsheet
+          </Nav.Link>
+          <Nav.Link as={Link} to="/stats">
+            Stats
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
-  </Navbar>
+    </Navbar>
   );
 }
 
