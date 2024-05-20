0d3b885329958ed62cca958ec708ed73caff2edf
KevinGage <dudeman02379@gmail.com>
2024-05-20 15:20:59 -0400

    renamed site

--- quabbin-website/app/layout.js
+++ quabbin-website/app/layout.js
@@ -5,8 +5,8 @@ import Navbar from "@/app/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Quabbin Records",
-  description: "Quabbin Opening Day Tournament Records",
+  title: "Quabbin Classic",
+  description: "Quabbin Classic Opening Day Tournament Records",
 };
 
 export default function RootLayout({ children }) {
